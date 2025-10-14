9df137e42a6e21aaee9b3b240c43597ac1d6bdbf
Orionpax <emil2008abdullaev@gmail.com>
2025-10-14 18:40:12 +0300

    Добавьте файлы проекта.

---
+++ Pr_13/Program.cs
@@ -0,0 +1,89 @@
+﻿//*******************************************************************************************
+//* Практическая работа №13                                                                 *
+//* Выполнил: Абдуллаев Э.С., группа 2-ИСПд                                                 *
+//* Задание: рекурсивная функция представления числа в обратном порядке                     *
+//*******************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static int ReverseNumber(int num, int result = 0)
+    {
+        if (num == 0)
+            return result;
+        int lastnumber = num % 10;
+        return ReverseNumber(num / 10, result * 10 + lastnumber);
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Clear();
+                Console.Title = "Практическая работа №13";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Здравствуйте!");
+                Console.Write("Введите число(a): ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    throw new Exception("Число(a) не может(могут) быть пустым!");
+                }
+                if (!int.TryParse(input, out int number))
+                {
+                    throw new Exception("Введите целое число!");
+                }
+                int reverse = ReverseNumber(number);
+                Console.WriteLine($"Перевернутое: {reverse}");
+            }
+            catch (FormatException fEx)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка:{fEx.Message}");
+                Console.ResetColor();
+            }
+            catch (OverflowException oEx)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {oEx.Message}");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                Console.ResetColor();
+            }
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1 - Новый поиск");
+            Console.WriteLine("0 - Выйти из программы");
+            Console.Write("Ваш выбор: ");
+            string choice = Console.ReadLine();
+            Console.ResetColor();
+            switch (choice)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Новый поиск...");
+                    break;
+                case "0":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Программа завершена.");
+                    Console.ReadKey();
+                    return;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Неверный выбор! Программа будет продолжена.");
+                    Console.ReadKey();
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
